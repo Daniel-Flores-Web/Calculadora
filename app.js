54556719e04673f73ded8baf0ab8b401e3458c10
xOnlinEx-Windows <daniel.floresvargas666@gmail.com>
2022-10-03 21:36:53 -0400

    adicionamos por primera vez html y css

---
+++ app.js
@@ -0,0 +1,41 @@
+document.querySelector('#number1').onclick = function(){
+    document.querySelector('#display').textContent = '1';
+}
+document.querySelector('#number2').onclick = function(){
+    document.querySelector('#display').textContent = '2';
+}
+document.querySelector('#number3').onclick = function(){
+    document.querySelector('#display').textContent = '3';
+}
+document.querySelector('#number4').onclick = function(){
+    document.querySelector('#display').textContent = '4';
+}
+document.querySelector('#number5').onclick = function(){
+    document.querySelector('#display').textContent = '5';
+}
+document.querySelector('#number6').onclick = function(){
+    document.querySelector('#display').textContent = '6';
+}
+document.querySelector('#number7').onclick = function(){
+    document.querySelector('#display').textContent = '7';
+}
+document.querySelector('#number8').onclick = function(){
+    document.querySelector('#display').textContent = '8';
+}
+document.querySelector('#number9').onclick = function(){
+    document.querySelector('#display').textContent = '9';
+}
+document.querySelector('#cero').onclick = function(){
+    document.querySelector('#display').textContent = '0';
+}
+document.querySelector('#suma').onclick = function(){
+    // if (document.querySelector('#display').textContent ===) {
+        
+    // }
+    document.querySelector('#display').textContent += '+';
+}
+document.querySelector('#operation-c').onclick = function(){
+    document.querySelector('#display').textContent = '0';
+}
+
+
