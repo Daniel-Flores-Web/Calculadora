e3765624c5023adaf777d87c64f0ae46fae09d7d
Daniel <daniel.floresvargas666@gmail.com>
2022-10-04 21:47:54 -0400

    se quito el boton power, se borro el anterior codigo js y se esta mejorando

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
-document.querySelector('#number1').onclick = function(){
-    document.querySelector('#display').textContent = '1';
-}
-document.querySelector('#number2').onclick = function(){
-    document.querySelector('#display').textContent = '2';
-}
-document.querySelector('#number3').onclick = function(){
-    document.querySelector('#display').textContent = '3';
-}
-document.querySelector('#number4').onclick = function(){
-    document.querySelector('#display').textContent = '4';
-}
-document.querySelector('#number5').onclick = function(){
-    document.querySelector('#display').textContent = '5';
-}
-document.querySelector('#number6').onclick = function(){
-    document.querySelector('#display').textContent = '6';
-}
-document.querySelector('#number7').onclick = function(){
-    document.querySelector('#display').textContent = '7';
-}
-document.querySelector('#number8').onclick = function(){
-    document.querySelector('#display').textContent = '8';
-}
-document.querySelector('#number9').onclick = function(){
-    document.querySelector('#display').textContent = '9';
-}
-document.querySelector('#cero').onclick = function(){
-    document.querySelector('#display').textContent = '0';
-}
-document.querySelector('#suma').onclick = function(){
-    // if (document.querySelector('#display').textContent ===) {
-        
-    // }
-    document.querySelector('#display').textContent += '+';
-}
-document.querySelector('#operation-c').onclick = function(){
-    document.querySelector('#display').textContent = '0';
-}
 
-
+document.getElementById('number1').addEventListener('click',() => {
+    (document.getElementById('display').textContent === '0')
+    ? document.getElementById('display').textContent = 1 
+    : document.getElementById('display').textContent += 1 
+});
+document.getElementById('suma').addEventListener('click',() => {
+    document.getElementById('display').textContent += '+';
+});
+document.getElementById('operation-c').addEventListener('click',() => {
+    document.getElementById('display').textContent = 0;
+});
+console.log(typeof document.getElementById('display').textContent)
